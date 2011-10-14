769ae5f44530b15c2d62fdb6f234c7f58e025044
mrodriguez@SW.ad <marcelo.rodriguez@southworks.net>
2011-10-14 08:56:25 -0300

    Added Begin 2 solution

---
+++ code/begin2/SocialGames.Web/Scripts/game/TicTacToeController.js
@@ -0,0 +1,61 @@
+﻿
+function TicTacToeController(viewModel, board, game) {
+    this.viewModel = viewModel;
+    this.board = board;
+    this.game = game;
+
+    var controller = this;
+
+    this.board.onMove = function (x, y) { controller.onMove(x, y); };
+};
+
+TicTacToeController.prototype.start = function (isFirst) {
+    if (isFirst) {
+        this.viewModel.playerColor(TTTColor.Cross);
+    }
+    else {
+        this.viewModel.playerColor(TTTColor.Circle);
+    }
+
+    this.viewModel.currentColor(TTTColor.Cross);
+};
+
+TicTacToeController.prototype.onMove = function (x, y) {
+    if (this.viewModel.playerColor() != this.viewModel.currentColor())
+        return;
+
+    var color = this.viewModel.currentColor();
+
+    if (!this.game.isValid(x, y, color))
+        return;
+
+    this.game.move(x, y, color);
+
+    this.board.drawMove(x, y, color);
+
+    this.updateGameStatus();
+};
+
+TicTacToeController.prototype.updateGameStatus = function () {
+
+    if (this.game.isTie()) {
+        this.viewModel.isTie(true);
+        this.viewModel.currentColor(TTTColor.Empty);
+        return;
+    }
+
+    if (this.game.hasWinner()) {
+        this.viewModel.winnerColor(this.game.getWinner());
+        this.viewModel.currentColor(TTTColor.Empty);
+        return;
+    }
+
+    this.viewModel.currentColor(this.game.nextColor(this.viewModel.currentColor()));
+
+    function nextColor(color) {
+        if (color === TTTColor.Cross)
+            return TTTColor.Circle;
+        if (color === TTTColor.Circle)
+            return TTTColor.Cross;
+    }
+};
