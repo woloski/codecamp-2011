0363651936f04897ab2871e0f935aaebf3bb80fa
woloski <woloski@sion.com>
2011-11-10 09:59:50 -0300

    # update to node 0.6 and changed the begin sln a bit

---
+++ code/tictactoe-end-eva/SocialGames.Web/Scripts/game/TicTacToeController.js
@@ -0,0 +1,79 @@
+﻿
+function TicTacToeController(viewModel, board, game, socket) {
+    this.viewModel = viewModel;
+    this.board = board;
+    this.game = game;
+    this.socket = socket;
+
+    this.socket.emit('join', 'singleroom');
+    this.socket.on('command', onNewCommand);
+    
+    var controller = this;
+
+    this.board.onMove = function (x, y) { controller.onMove(x, y); };
+
+
+    function onNewCommand(gameAction) {
+        if (gameAction.Type != 1)
+            return;
+
+        var x = parseInt(gameAction.CommandData.x);
+        var y = parseInt(gameAction.CommandData.y);
+        var color = gameAction.CommandData.color;
+
+        if (!controller.game.isValid(x, y, color))
+            return;
+
+        controller.game.move(x, y, color);
+        controller.board.drawMove(x, y, color);
+
+        controller.updateGameStatus();
+    }
+
+        
+};
+        
+
+TicTacToeController.prototype.start = function () {
+    this.viewModel.currentColor(TTTColor.Cross);
+};
+
+TicTacToeController.prototype.onMove = function (x, y) {
+    var color = this.viewModel.currentColor();
+
+    if (!this.game.isValid(x, y, color))
+        return;
+
+    this.game.move(x, y, color);
+
+    this.board.drawMove(x, y, color);
+
+    this.updateGameStatus();
+
+    var action = { Type: 1, CommandData: { 'x': x, 'y': y, 'color': color} };
+    this.socket.emit('command', action);
+};
+
+TicTacToeController.prototype.updateGameStatus = function () {
+
+    if (this.game.isTie()) {
+        this.viewModel.isTie(true);
+        this.viewModel.currentColor(TTTColor.Empty);
+        return;
+    }
+
+    if (this.game.hasWinner()) {
+        this.viewModel.winnerColor(this.game.getWinner());
+        this.viewModel.currentColor(TTTColor.Empty);
+        return;
+    }
+
+    this.viewModel.currentColor(this.game.nextColor(this.viewModel.currentColor()));
+
+    function nextColor(color) {
+        if (color === TTTColor.Cross)
+            return TTTColor.Circle;
+        if (color === TTTColor.Circle)
+            return TTTColor.Cross;
+    }
+};
---
+++ code/tictactoe-end-eva/SocialGames.Web/Scripts/game/TicTacToeViewModel.js
@@ -0,0 +1,26 @@
+﻿
+TTTColor = { Empty: 0, Cross: 1, Circle: 2 };
+
+
+function TicTacToeViewModel() {
+    this.playerColor = ko.observable(TTTColor.Empty);
+    this.winnerColor = ko.observable(TTTColor.Empty);
+    this.currentColor = ko.observable(TTTColor.Empty);
+    this.isTie = ko.observable(false);
+    this.controller = null;
+
+    this.startGame = function () {
+        var canvas = document.getElementById("board");
+        var board = new TicTacToeBoard(canvas);
+        var game = new TicTacToeGame();
+
+        var socket = io.connect('http://localhost:8080');
+
+        this.controller = new TicTacToeController(this, board, game, socket);
+
+        this.controller.start();
+    };
+}
+
+
+
